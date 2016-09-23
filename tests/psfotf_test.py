9fd3c88cb9905b431d80fd5dfc97e10981f7aa4b
David Hoffman <dave.p.hoffman@gmail.com>
2016-09-23 18:38:49 -0400

    Added tests for HanserPSF

---
+++ tests/psfotf_test.py
@@ -0,0 +1,25 @@
+from nose.tools import *
+import unittest
+from psfotf import *
+
+
+class TestPupil(unittest.TestCase):
+    """Test HanserPSF"""
+
+    def setUp(self):
+        self.model = HanserPSF(500, 0.85, 1.0, 140, 200)
+
+    def test_size(self):
+        '''
+        Make sure when size is changed the output changes accordingly
+        '''
+
+        model = self.model
+
+        model.size = 128
+        model.zrange = [-1000, 0, 1000]
+        assert_tuple_equal(model.PSFi.shape, (3, 128, 128))
+
+        model.size = 512
+        model.zrange = 0
+        assert_tuple_equal(model.PSFi.shape, (1, 512, 512))
