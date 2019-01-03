002fed58660839692e5b4652c80bd61334133136
David Hoffman <dave.p.hoffman@gmail.com>
2019-01-03 17:31:29 -0500

    Thin labview wrapper for phaseretrieval

--- tests/integration_tests.py
+++ tests/integration_tests.py
@@ -1,3 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# integration_tests.py
+"""
+Small test suite
+
+Copyright David Hoffman, 2018
+"""
+
 from nose.tools import *
 import unittest
 from pyOTF.otf import *
