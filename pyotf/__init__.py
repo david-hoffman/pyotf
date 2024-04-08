a120837f49b39c6e1d9e9d23634ac0cf8be15b5e
David Hoffman <dave.p.hoffman@gmail.com>
2024-04-08 13:56:35 -0700

    another version fix

--- pyotf/__init__.py
+++ pyotf/__init__.py
@@ -11,11 +11,6 @@ https://en.wikipedia.org/wiki/Phase_retrieval
 Copyright (c) 2016, David Hoffman
 """
 
-from ._version import get_versions
-
-__version__ = get_versions()["version"]
-del get_versions
-
 from . import _version
 
 __version__ = _version.get_versions()["version"]
